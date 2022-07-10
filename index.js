00bd7915e914d181652864da64b054af2e6e9d81
Mostafa Milani Amin <90893927+Mostafa-Milani-Amin@users.noreply.github.com>
2022-07-10 12:06:39 +0430

    Name change.

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const deleteManyFiles = pathArray => {
+const removeManyFiles = pathArray => {
     for (let i = 0; i < pathArray.length; i++) {
         require("fs").unlink(pathArray[i], (err) => err ? console.error(err) : console.log("File removed:", pathArray[i]))
 
     }
 }
 
-module.exports = { deleteManyFiles }
+module.exports = { removeManyFiles }
